b607b9c75d3166e81c8300f02bdb46ddb6ad2c20
murotani <mu.1ok.san@gmail.com>
2020-06-29 12:37:27 +0900

    数当てゲームの流れを記述

---
+++ kazuate/Kazuate.java
@@ -0,0 +1,29 @@
+package hello.kazuate;
+
+public class Kazuate {
+    // プログラムが設定するランダムな2桁の正の整数
+    private static int randomNumber;
+
+    // プレイヤーが数値を入力できる回数
+    private static final int PLAY_NUM = 5;
+
+    /**
+     * メイン関数
+     */
+    public static void main( String[] args ) {
+        System.out.println( "数当てゲームを始めます．" );
+        System.out.println( "5回以内に当ててください．" );
+        System.out.println( "----------------------------------" );
+
+        // ランダムな数値を決定する
+
+        // プレイヤーの入力を受ける
+
+        // プレイヤーの入力を精査する
+
+        // 勝利判定
+
+        System.out.println( "数当てゲームを終了します．" );
+        return;
+    }
+}
